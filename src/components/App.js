529e107f347408bafcc9bc61e347458080fad6e1
Priyanka-Pol <priyankapol2019@gmail.com>
2023-07-17 12:31:08 +0530

    submitting code-Mon Jul 17 2023 12:31:06 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let [a, setA] = useState(0)
+  function bnt() {
+    setA(a+1)
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h1>Button clickes {a} timesa</h1>
+
+        <button onClick = {btn}>Click Me</button>
     </div>
   )
 }
